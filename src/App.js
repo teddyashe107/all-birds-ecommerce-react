22608c5e4962be0ca78727c88410c42b302a2845
teddyashe107 <teddyashe107@gmail.com>
2023-01-13 15:19:27 +0300

    init

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1 className='text-red-700' > tailwindcss with react</h1>
+    </div>
+  );
+}
+
+export default App;

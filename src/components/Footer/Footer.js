9acd502d56af576af1e1122f3641a5d7bb5a4e6f
teddyashe107 <teddyashe107@gmail.com>
2023-01-30 18:17:39 +0300

    done with refactoring footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,107 +1,9 @@
 import ListItem from './ListItem';
 import ImageList from './ImageList';
+import footerData from './footer.json'
+import socials from './socials.json'
+import flags from './flags.json'
 
-const help = {
-	title: 'help',
-	items: [
-		'1-888-963-8944',
-		'1-814-251-9966 (Text)',
-		'help@allbirds.com',
-		'Returns/Exchanges',
-		'FAQ/Contact Us',
-		'Afterpay',
-	],
-};
-
-const shop = {
-	title: 'Shop',
-	items: [
-		"Men's Shoes",
-		"Women's Shoes",
-		"Men's Apparel",
-		"Women's Apparel",
-		'Socks',
-		'Gift Cards',
-		'Refer a Friend',
-		'Download the Allbirds App',
-	],
-};
-const components = {
-	title: 'company',
-	items: [
-		'Our Stores',
-		'Our Story',
-		'Our Materials',
-		'Sustainability',
-		'FAQ/Contact Us',
-		'Our Stores',
-		'Our Story',
-		'Our Materials',
-		'Sustainability',
-		'FAQ/Contact Us',
-	],
-};
-
-const data = [
-	{
-		title: 'help',
-		items: [
-			'1-888-963-8944',
-			'1-814-251-9966 (Text)',
-			'help@allbirds.com',
-			'Returns/Exchanges',
-			'FAQ/Contact Us',
-			'Afterpay',
-		],
-	},
-	{
-		title: 'Shop',
-		items: [
-			"Men's Shoes",
-			"Women's Shoes",
-			"Men's Apparel",
-			"Women's Apparel",
-			'Socks',
-			'Gift Cards',
-			'Refer a Friend',
-			'Download the Allbirds App',
-		],
-	},
-	{
-		title: 'company',
-		items: [
-			'Our Stores',
-			'Our Story',
-			'Our Materials',
-			'Sustainability',
-			'FAQ/Contact Us',
-			'Our Stores',
-			'Our Story',
-			'Our Materials',
-			'Sustainability',
-			'FAQ/Contact Us',
-		],
-	},
-];
-
-const socials = [
-	'https://cdn.allbirds.com/image/upload/q_auto/cms/7ALf4NFNH4VStmrXU0qSKK/0aa03178ac1a2dc0789703f6dac6e7ef/social-instagram.svg',
-	'https://cdn.allbirds.com/image/upload/q_auto/cms/7jsrb2oMQypD1kO6iZLXZN/8bbcbd3f4c776e7150954f053aee3c00/social-twitter-white.svg',
-	'https://cdn.allbirds.com/image/upload/q_auto/cms/55ZLXdMQu728huA78Qlfkb/faa7706d3b83f77d73026e3cfdaca7d0/social-facebook.svg',
-	'https://cdn.allbirds.com/image/upload/q_auto/cms/5VBkylibm5XkgzjDP153Mo/e3d80019f111cf8652618aa05ca5528e/social-pinterest-white.svg',
-];
-
-const flags = [
-	'https://cdn.allbirds.com/image/upload/q_auto/cms/3ohqY3WWyehlkYpVi815p9/6f7fbf2f1e4a08abc8da4788a95c2376/flag-us.svg',
-	'https://cdn.allbirds.com/image/upload/q_auto/cms/22gqFXi2QPoT2IhEpBuzOi/d01a77d603e94a10b8a690deded124f7/flag-nz.svg',
-	'https://cdn.allbirds.com/image/upload/q_auto/cms/6FnrOphj9NsjD38uREtsBn/cbf3c42e92a6c0628da4462b04e92e2c/flag-au.svg',
-	'https://cdn.allbirds.com/image/upload/q_auto/cms/mpzswQHoLPhzgFkXpcZ7P/eba356c7c7929352d5d3eea6999f5ad3/flag-ca.svg',
-	'https://cdn.allbirds.com/image/upload/q_auto/cms/5OVl6dmwlwOJPrg5F3Ot1s/e9679772ddd64de49cd96cbd4a819743/flag-uk.svg',
-	'https://cdn.allbirds.com/image/upload/q_auto/cms/2bpMfDa3leuCSpX1lLXKvB/19c74fd77f850e29497ad82d91e83292/flag-cn.svg',
-	'https://cdn.allbirds.com/image/upload/q_auto/cms/5DEtULZUsBHTJp8aXcaOIc/c9d3ff14bde3bf45235f906dfb63f649/method-draw-image.svg',
-	'https://cdn.allbirds.com/image/upload/q_auto/cms/3TWBjQxibzqkiXua4hrmCu/1730ec7122e70b26bd29d972733a8190/Japan-Flag.svg',
-	'https://cdn.allbirds.com/image/upload/q_auto/cms/lqp21Rk7GI8HTImpSTOip/cdb1a8e18b337c27e91c18b82dd93ddf/South-Korea-Flag.svg',
-];
 
 const Footer = () => {
 	return (
@@ -127,13 +29,13 @@ const Footer = () => {
 			<div className="grid gap-5 my-10 text-center lg:text-left lg:grid-cols-4">
 				<div className="lg:col-span-2">
 					<div className="lg:grid lg:grid-cols-2">
-						<ListItem content={help} />
-						<ListItem content={shop} />
+						<ListItem content={footerData[0]} />
+						<ListItem content={footerData[1]} />
 					</div>
 				</div>
 				<div className="lg:col-span-2">
 					{' '}
-					<ListItem content={components} />
+					<ListItem content={footerData[2]} />
 				</div>
 			</div>
 
